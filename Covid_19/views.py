3f8ca5223f9186d40861ab0e3ac6b24106641a66
Panchal Pratik <panchalpratik1998@gmail.com>
2020-05-18 19:53:51 +0530

    First Commit

---
+++ Covid_19/views.py
@@ -0,0 +1,56 @@
+import json
+from urllib.request import urlopen
+from django.shortcuts import render, HttpResponse
+# from .models import Product
+import requests
+import datetime
+from datetime import timedelta
+import get
+
+
+def index(request):
+    # products = Product.objects.all()
+    # return render(request, 'index.html', {'products': products})
+
+    # querystring = {"format": "json"}
+    url = "https://corona-virus-world-and-india-data.p.rapidapi.com/api_india"
+    headers = {
+        'x-rapidapi-host': "corona-virus-world-and-india-data.p.rapidapi.com",
+        'x-rapidapi-key': "fb3acd48b9mshbc47c3a4281bd66p105528jsncd3837fd509a"
+    }
+
+    response = requests.request(
+        "GET", url, headers=headers)
+
+    totalData = response.json()
+
+    jsonUrl2 = "https://api.covid19india.org/data.json"
+    stateData = json.load(urlopen(jsonUrl2))
+
+    currentTime = datetime.datetime.now()
+
+    all_data = {
+        'stateData': stateData,
+        'currentTime': currentTime,
+        'totalData': totalData,
+    }
+
+    return render(request, 'index.html', all_data)
+
+
+def distData(request):
+    query = request.GET.get('state_name')
+
+    jsonUrl = 'https://api.covid19india.org/v2/state_district_wise.json'
+    distData = json.load(urlopen(jsonUrl))
+
+    jsonUrl3 = "https://api.covid19india.org/zones.json"
+    zonesData = json.load(urlopen(jsonUrl3))
+
+    sub_data = {
+        'distData': distData,
+        'zonesData': zonesData,
+        'query': query,
+    }
+
+    return render(request, 'dist.html', sub_data)
---
+++ static/script.js
@@ -0,0 +1,14 @@
+// let body = document.querySelector("#body");
+// let clickstatevalue = document.querySelector("#stateClick");
+
+function showDistData(distId) {
+  if (distId.style.display === "none") {
+    distId.style.display = "block";
+    // body.style.opacity = "0.1";
+  } else {
+    distId.style.display = "none";
+    // body.style.opacity = "1";
+  }
+}
+
+// clickstate.onclick = distData;
